32ef5896e1a23d8b8e9476dcf6e4034d209074b2
Rachel Thomas <120598085+r-thomas10@users.noreply.github.com>
2023-04-27 18:30:50 -0400

    Add files via upload

---
+++ counting loops/script.js
@@ -0,0 +1,19 @@
+for (i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+for (i = 10; i >= 1; i--) {
+  console.log(i);
+}
+
+let count = 1;
+
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+while (i >= 1) {
+  console.log(i);
+  i--;
+}
